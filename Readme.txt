906bcb3f1b3549a76ff7b292fa6ad5072a52e2f8
abhilash <abhilash.sixthsense@gmail.com>
2021-07-17 13:22:29 +0530

    Adding docker file for python client program.

---
+++ Readme.txt
@@ -0,0 +1,12 @@
+Setup
+-------
+1)Install docker compose
+    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+
+2)Build the testclient
+    cd to testclient directory
+    sudo docker build -t python-test-client .
+
+3)cd back to the parent directory and run the docker compose and all the services will be started
+    sudo docker-compose up.
--- testclient/test.py
+++ testclient/test.py
@@ -9,7 +9,7 @@ print('hi')
 # myclient = pymongo.MongoClient(
 #     "mongodb://localhost:40001/", replicaset='myrepl')
 myclient = pymongo.MongoClient(
-    ["mongodb://localhost:40001/", "mongodb://localhost:40002/", "mongodb://localhost:40003/"])
+    ["mongodb://repl1:27017/", "mongodb://repl2:27017/", "mongodb://repl3:27017/"])
 mydb = myclient["mydatabase"]
 mycol = mydb["customers"]
 print(myclient.list_database_names())
@@ -22,13 +22,16 @@ def insert():
 
 
 def query():
+
     print(mycol.count_documents({}))
 
 
 if __name__ == '__main__':
+    print('Running Test')
     while(True):
         try:
             # insert()
+            print('Running Query')
             query()
 
         except Exception as e:
