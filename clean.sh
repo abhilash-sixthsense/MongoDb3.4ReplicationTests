07f861fb388858ed332c1cafa18e4a72f228b690
abhilash <abhilash.sixthsense@gmail.com>
2021-07-27 13:28:30 +0530

    added Clean  from missing volumes and images.

--- clean.sh
+++ clean.sh
@@ -1,2 +1,2 @@
-docker container rm repl1 repl2 repl3 python-test-client
-docker volume rm config-server_repl1 config-server_repl2 config-server_repl3
+docker container rm repl1 repl2 repl3 python-test-client cfgsvr1 cfgsvr2 cfgsvr3
+docker volume rm config-server_repl1 config-server_repl2 config-server_repl3 configserver_cfgsvr1 configserver_cfgsvr2 configserver_cfgsvr3
